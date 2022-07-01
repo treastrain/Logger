242c2fa2b7c0fae4f97c4f73f32a48f06caae950
treastrain / Tanaka Ryoga <tr@tret.jp>
2022-07-02 04:26:11 +0900

    Add a developer-friendly message on how to use it

--- Sources/Logger.swift
+++ Sources/Logger.swift
@@ -10,9 +10,16 @@ import Foundation
 public enum Logger {
     public typealias Category = AnyHashable
     
-    public static func configure(bundleIdentifier: String = Bundle.main.bundleIdentifier!) {
+    public static func configure() {
+        guard let bundleIdentifier = Bundle.main.bundleIdentifier else {
+            fatalError("`Bundle.main.bundleIdentifier` is nil. Please pass a valid bundle identifier.")
+        }
+        configure(bundleIdentifier: bundleIdentifier)
+    }
+    
+    public static func configure(bundleIdentifier: String) {
         guard Self.bundleIdentifier == nil else {
-            return logger.log(level: .fault, "Logger's bundle identifier has already been configured.")
+            return logger.log(level: .fault, "Logger's bundle identifier has already been configured. The bundle identifier \(bundleIdentifier, privacy: .public) just given is not set.")
         }
         
         Self.bundleIdentifier = bundleIdentifier
@@ -22,13 +29,16 @@ public enum Logger {
         if let logger = loggers[category] {
             return logger
         } else {
+            guard let bundleIdentifier = bundleIdentifier else {
+                fatalError("`bundleIdentifier` has not been set. The default bundle identifier has not yet been configured. Call `Logger.configure()` before calling `Logger.\(#function)`.")
+            }
             let logger = os.Logger(subsystem: bundleIdentifier, category: category.description)
             loggers[category] = logger
             return logger
         }
     }
     
-    private static var bundleIdentifier: String!
+    private static var bundleIdentifier: String?
     private static var loggers: [Category : os.Logger] = [:]
     private static var logger: os.Logger { logger() }
 }
